901c14b941b055b07e6ac4c0f15712f34ae676a4
ndevu3399 <wachiramwas122@gmail.com>
2025-03-10 13:51:51 +0300

    mody index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// Function Declaration: saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function Expression: mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Exporting the functions for testing
+  module.exports = { saturdayFun, mondayWork };
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Exporting the function if needed
+  module.exports = { wrapAdjective };
+  
